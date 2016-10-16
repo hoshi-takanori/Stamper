0778a29a5fdac7eedfb5042b1de115d242ab516c
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2016-10-16 09:33:58 +0900

    押された点を描画

--- app/src/main/java/com/example/stamper/StamperView.java
+++ app/src/main/java/com/example/stamper/StamperView.java
@@ -1,11 +1,18 @@
 package com.example.stamper;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class StamperView extends View {
+
+    private Bitmap bitmap;
+    private Canvas bitmapCanvas;
 
     public StamperView(Context context) {
         super(context);
@@ -20,6 +27,31 @@ public class StamperView extends View {
     }
 
     @Override
+    protected void onSizeChanged(int width, int height, int oldw, int oldh) {
+        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmapCanvas = new Canvas(bitmap);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, 0, 0, null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        int action = event.getAction();
+
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE) {
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(20);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            bitmapCanvas.drawPoint(x, y, paint);
+            invalidate();
+        }
+
+        return true;
     }
 }
